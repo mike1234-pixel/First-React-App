a4434042764e37c5e8488e52cd2bdb2ccce472cb
Michael Tandy <mike.tandy.1@gmail.com>
2020-06-21 19:42:17 +0100

    Updates

---
+++ precache-manifest.d889fec505fedb94a8b0622f71485984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4292ab8e1aa8db9cbd6c97b1bd9516e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e82ba9d648b73182ca20",
+    "url": "/static/css/main.53758901.chunk.css"
+  },
+  {
+    "revision": "2d65b7fe05165d69d176",
+    "url": "/static/js/2.08989de2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.08989de2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e82ba9d648b73182ca20",
+    "url": "/static/js/main.e8f4a964.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
